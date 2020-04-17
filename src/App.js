97f4f5d0b0afe3cccff6f4e557ecccc078d37274
R@HUL <31180528+guntharahul@users.noreply.github.com>
2020-04-17 12:29:42 +0530

    Ready Code for deployment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './App.css';
 import NavBar from './components/layout/NavBar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import About from './components/pages/About';
 import User from './components/users/User';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 
 const App = () => {
   // async componentDidMount() {
@@ -35,18 +35,10 @@ const App = () => {
             <div className='container'>
               <Alert></Alert>
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search></Search>
-                      <Users></Users>
-                    </Fragment>
-                  )}
-                ></Route>
+                <Route exact path='/' component={Home}></Route>
                 <Route exact path='/about' component={About}></Route>
                 <Route exact path='/user/:login' component={User}></Route>
+                <Route component={NotFound}></Route>
               </Switch>
             </div>
           </div>
